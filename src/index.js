d8984e8ac26337eee0eda451454883c77e36bace
Adriele <adriele.dejprimo@gmail.com>
2024-06-21 17:52:12 -0300

    Adding try and catch

--- src/index.js
+++ src/index.js
@@ -15,9 +15,13 @@ client.on('ready', ()=>{
 const bomDiaRegex = /\bbom\s*dia\b/i;
 
 client.on('message', async(message) => {
+    try{
     if (bomDiaRegex.test(message.body))  {
          await message.reply(`Bom dia ${emoji.get(':heart:')}`); 
          logger.info(`Replied to Bom dia message from ${message.from}`);
 }   
+    }catch(error){
+        logger.error(`Error handling message:${error}`);
+    }
 });
 client.initialize();
