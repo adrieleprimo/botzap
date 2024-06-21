2f5b5e60f51d85d280c8fe214aad8efa9b903579
Adriele <adriele.dejprimo@gmail.com>
2024-06-21 17:42:20 -0300

    Creating a logging with Winston

---
+++ src/winstonLogger.js
@@ -0,0 +1,12 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports:[
+        new winston.transports.File({filename: 'bot.log'}),
+        new winston.transports.Console()
+    ],
+});
+
+module.exports = logger;
