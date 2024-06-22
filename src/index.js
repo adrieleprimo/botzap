7dd41ab271e1bd1e1b14cc9e0c1048af9622888f
Adriele <adriele.dejprimo@gmail.com>
2024-06-22 12:32:40 -0300

    Personalizing the good morning

--- src/index.js
+++ src/index.js
@@ -16,12 +16,17 @@ const bomDiaRegex = /\bbom\s*dia\b/i;
 
 client.on('message', async(message) => {
     try{
-    if (bomDiaRegex.test(message.body))  {
-         await message.reply(`Bom dia ${emoji.get(':heart:')}`); 
-         logger.info(`Replied to Bom dia message from ${message.from}`);
-}   
-    }catch(error){
+        let contact = (await client.getContactById(message.from)).name;
+        if (bomDiaRegex.test(message.body)){
+            let replyMessage = `Bom dia ${contact}${emoji.get(':heart:')}`;
+            if(!contact){
+                replyMessage = `Bom dia ${emoji.get(':heart:')}`;
+            }
+            await message.reply(replyMessage); 
+            logger.info(`Replied to Bom dia message from ${message.from}`); 
+    }        
+}catch(error){
         logger.error(`Error handling message:${error}`);
-    }
+}
 });
 client.initialize();
