af63a0f1ed90b9ae7c4ad76993406faceaa5970f
Adriele <adriele.dejprimo@gmail.com>
2024-06-26 19:39:27 -0300

    Organizing the filename and folder of winston

--- src/winstonLogger.js
+++ src/winstonLogger.js
@@ -4,7 +4,7 @@ const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     transports:[
-        new winston.transports.File({filename: 'bot.log'}),
+        new winston.transports.File({filename: 'logs/bot.log'}),
         new winston.transports.Console()
     ],
 });
